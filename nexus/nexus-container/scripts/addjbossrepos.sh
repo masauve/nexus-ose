22da4702f7ecd3b6fe163c55f8c44904706d74b3
Jochen Cordes <jcordes@redhat.com>
2016-01-21 21:00:07 +0100

    Changed image-streams, added additional repos.

--- nexus/nexus-container/scripts/addjbossrepos.sh
+++ nexus/nexus-container/scripts/addjbossrepos.sh
@@ -4,3 +4,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 ${DIR}/addrepo.sh jboss http://maven.repository.redhat.com/techpreview/all/
 ${DIR}/addrepo.sh jboss-ce https://repository.jboss.org/nexus/content/groups/public/
+${DIR}/addrepo.sh fusesource-public https://repo.fusesource.com/nexus/content/groups/public
+${DIR}/addrepo.sh fusesource-ea https://repo.fusesource.com/nexus/content/groups/ea
+${DIR}/addrepo.sh redhat-ga https://maven.repository.redhat.com/ga
+${DIR}/addrepo.sh apache-public https://repo.maven.apache.org/maven2
