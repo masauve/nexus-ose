1cee2dae50037ea1dc5e7cb82b3dbadd6fd7b6bc
Jochen Cordes <jcordes@redhat.com>
2016-01-23 13:07:46 +0100

    Removed repos from nexus.xml and create them via script from now on.

--- nexus/nexus-container/scripts/addjbossrepos.sh
+++ nexus/nexus-container/scripts/addjbossrepos.sh
@@ -2,6 +2,9 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+${DIR}/addrepo.sh central https://repo1.maven.org/maven2/
+${DIR}/addrepo.sh apache-snapshots https://repository.apache.org/snapshots/
+${DIR}/addrepo.sh codehaus-snapshots</id> <url>https://nexus.codehaus.org/snapshots/
 ${DIR}/addrepo.sh jboss http://maven.repository.redhat.com/techpreview/all/
 ${DIR}/addrepo.sh jboss-ce https://repository.jboss.org/nexus/content/groups/public/
 ${DIR}/addrepo.sh fusesource-public https://repo.fusesource.com/nexus/content/groups/public/
